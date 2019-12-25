97c487482aa00cc2baf7b874ab2abd08604a1b3c
spencer-gray <spencer.p.gray@gmail.com>
2019-12-25 16:14:02 -0500

    adding company info component

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import { TextField } from '@material-ui/core';
-import { withStyles } from '@material-ui/styles';
+import SearchBox from './components/SearchBox/SearchBox.js';
+import CompanyInfo from './components/CompanyDetails/CompanyHeader';
 //import SearchIcon from "@material-ui/icons/Search";
 
 const WAIT_INTERVAL = 1000;
 const ENTER_KEY = 13;
 
-const CssTextField = withStyles({
-  root: {
-    // '& label.Mui-focused': {
-    //   color: 'green',
-    // },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: '#FFFFFF',
-      },
-      '&:hover fieldset': {
-        borderColor: '#FFFFFF',
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: '#FFFFFF',
-      },
-    },
-  },
-})(TextField);
-
 class App extends Component {
   state = {
     stockTicker: ''
-  }
+  };
 
   timer = null
 
   // searches when user is done typing (wait-time determined by WAIT_INTERVAL)
   handleChange = e => {
     clearTimeout(this.timer)
-
-    this.setState({ stockTicker: e.target.value })
-
-    this.timer = setTimeout(this.triggerChange, WAIT_INTERVAL)
+    this.setState({ stockTicker: e.target.value });
+    this.timer = setTimeout(this.triggerChange, WAIT_INTERVAL);
   }
 
   // searches when user clicks enter
@@ -58,32 +37,17 @@ class App extends Component {
 
   render(){
       return (
-          <div className="searchBox">
-              <CssTextField
-              //id="custom-css-outlined-input"
-              label="Stock Ticker..."
-              variant="outlined"
-              value={this.state.value}
-              onChange={this.handleChange}
-              onKeyDown={this.handleKeyDown}
-              fullWidth
-              InputLabelProps={{style: { textAlign: 'center', fontSize: 20, color: '#FFFFFF'}}}
-              inputProps={{ min: 0,
-                            maxLength: 5, 
-                            style: { textAlign: 'center', fontSize: 20, color: '#FFFFFF', textTransform: 'uppercase'},
-                          }}
-              // (setup for search button)
-              // InputProps={{
-              //     endAdornment: (
-              //     <InputAdornment>
-              //         <IconButton>
-              //         <SearchIcon color='primary'/>
-              //         </IconButton>
-              //     </InputAdornment>
-              //     )
-              // }}
-              />
+        <div>
+          <SearchBox 
+            stockTicker={this.state.stockTicker}
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
+          />
+          <div className="row1">
+            <CompanyInfo />
           </div>
+        </div>
+          
       );
   }
 }
---
+++ client/src/components/SearchBox/SearchBox.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import './SearchBox.css';
+import { TextField } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
+//import SearchIcon from "@material-ui/icons/Search";
+
+// const WAIT_INTERVAL = 1000;
+// const ENTER_KEY = 13;
+
+const CssTextField = withStyles({
+  root: {
+    // '& label.Mui-focused': {
+    //   color: 'green',
+    // },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#ffffff',
+      },
+      '&:hover fieldset': {
+        borderColor: '#ffffff',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#ffffff',
+      },
+    },
+  },
+})(TextField);
+
+class SearchBox extends Component {
+//   state = {
+//     value: this.props.value
+//   }
+
+  //handler = this.handler.bind(this);
+
+  timer = null
+
+//   handler(e) {
+
+//     clearTimeout(this.timer)
+
+//     this.setState({ value: e.target.value });
+
+//     //this.props.setState({ value: e.target.value });
+
+//     this.timer = setTimeout(this.triggerChange, WAIT_INTERVAL);
+
+//     /// delete below
+//     // console.log(event.target.value);
+
+//     // this.setState({value: event.target.value});
+//   }
+
+  // searches when user is done typing (wait-time determined by WAIT_INTERVAL)
+//   handleChange = e => {
+//     clearTimeout(this.timer)
+
+//     this.setState({ value: e.target.value });
+
+//     //this.props.setState({ value: e.target.value });
+
+//     this.timer = setTimeout(this.triggerChange, WAIT_INTERVAL);
+//   }
+
+//   // searches when user clicks enter
+//   handleKeyDown = e => {
+//     if (e.keyCode === ENTER_KEY) {
+//       clearTimeout(this.timer)
+//       this.triggerChange()
+//     }
+//   }
+
+//   // executes search (just logging to console for now)
+//   triggerChange = () => {
+//     const { value } = this.state
+//     console.log(value.toUpperCase());
+//     // props function was here
+//     //this.props.onChange();
+//   }
+
+  render(){
+      return (
+          <div className="searchBox">
+            <CssTextField
+                //id="custom-css-outlined-input"
+                label="Stock Ticker..."
+                variant="outlined"
+                value={this.props.value}
+                onChange={this.props.onChange}
+                onKeyDown={this.props.onKeyDown}
+                fullWidth
+                InputLabelProps={{style: { textAlign: 'center', fontSize: 20, color: '#FFFFFF'}}}
+                inputProps={{ min: 0,
+                                maxLength: 5, 
+                                style: { textAlign: 'center', fontSize: 20, color: '#FFFFFF', textTransform: 'uppercase'},
+                            }}
+                // (setup for search button)
+                // InputProps={{
+                //     endAdornment: (
+                //     <InputAdornment>
+                //         <IconButton>
+                //         <SearchIcon color='primary'/>
+                //         </IconButton>
+                //     </InputAdornment>
+                //     )
+                // }}
+            />
+          </div>
+      );
+  }
+}
+
+export default SearchBox
