33f7eeda35703a5a42c174070759a6d6c63da991
spencer-gray <spencer.p.gray@gmail.com>
2020-01-03 14:08:47 -0500

    Fix for deployment, accessing client build folder

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ if(process.env.NODE_ENV === 'development') {
 // Profile routes
 app.use('/api/v1/stock', require('./routes/profile'))
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
